e0c1eabfdd6a1f90eca45a7139e964d99f5bc752
Voidruiyi <739368732@qq.com>
2019-08-24 13:36:19 +0800

    dev_1:1336

--- app/src/main/java/com/ruiyi/testioc/view/MainActivity.java
+++ app/src/main/java/com/ruiyi/testioc/view/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends InJectActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mTextView.setText("1155");
+        mTextView.setText("1336");
     }
 }
