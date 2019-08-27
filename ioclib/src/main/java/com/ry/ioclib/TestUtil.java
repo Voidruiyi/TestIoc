b724e5f901a1a49eafc635d49a1de9d4b0a62b55
Voidruiyi <739368732@qq.com>
2019-08-27 17:52:42 +0800

    dev_1:commot_1413-2

---
+++ ioclib/src/main/java/com/ry/ioclib/TestUtil.java
@@ -0,0 +1,16 @@
+package com.ry.ioclib;
+
+/**
+ * Created by ruiyi on 2019/8/27.
+ */
+
+public class TestUtil {
+
+    public static int getCount(int c){
+        if(c==4){
+            return 9;
+        }
+
+        return c+22;
+    }
+}
